a7142d3d57d123250aed58ebbe6ee3d1c9e5916f
Bret Finley <heinousparadigm@gmail.com>
2015-11-25 10:19:38 -0700

    Deleted some stuff

--- public/SteamCalculator-Scripts-master/sql/steamcalculator-scripts.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS `sc_steamgames` (
-  `appid` int(10) unsigned NOT NULL,
-  `title` varchar(250) NOT NULL,
-  `price` mediumint(6) unsigned NOT NULL,
-  PRIMARY KEY (`appid`),
-  KEY `title` (`title`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
