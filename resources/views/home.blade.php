10d77f5448678b5b9235421f4b8fee63c6f4064f
Bret Finley <heinousparadigm@gmail.com>
2015-12-02 15:43:16 -0700

    Added newline

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,10 +10,10 @@
     <p class="news-content">
         Before you get started, make sure that you've set everything in your Steam
         profile to "Public". This helps ensure that we can make those API confidently
-        and effectively! If you need help on the process, please check out the <a href="/help">help page</a>.<br />
+        and effectively! If you need help on the process, please check out the <a href="/help">help page</a>.<br /><br />
         All you need to log in is your Steam ID, it's really that simple! No passwords or usernames, and no pesky confimartion
         emails either! Really! All you need to do is grab your unique Steam ID and paste it into the bar over there. If you need
-        help on finding your Steam ID, try Googling it, or head over to our <a href="/help">help page</a>. 
+        help on finding your Steam ID, try Googling it, or head over to our <a href="/help">help page</a>.
     </p>
 </div>
 
