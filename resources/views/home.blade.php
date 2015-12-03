e2f8f75275579e97babf83b8b5f1378bcbd1735b
Bret Finley <heinousparadigm@gmail.com>
2015-12-03 14:32:18 -0700

    Added my SteamID

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,8 @@
         and effectively! If you need help on the process, please check out the <a href="/help">help page</a>.<br /><br />
         All you need to log in is your Steam ID, it's really that simple! No passwords or usernames, and no pesky confimartion
         emails either! Really! All you need to do is grab your unique Steam ID and paste it into the bar over there. If you need
-        help on finding your Steam ID, try Googling it, or head over to our <a href="/help">help page</a>.
+        help on finding your Steam ID, try Googling it, or head over to our <a href="/help">help page</a>.<br /><br />
+        Bret's Steam ID: 76561198025369330
     </p>
 </div>
 
